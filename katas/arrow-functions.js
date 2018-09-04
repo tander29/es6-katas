b8447fbabe7c9549ce45e4f3ceeaefacb2d9539b
Travis Anderson <thetanderson@gmail.com>
2018-09-04 14:20:35 -0400

    think i have the arrow function katas done

--- katas/arrow-functions.js
+++ katas/arrow-functions.js
@@ -1,26 +1,28 @@
 /* 
  * 1. Convert this to an arrow function
  */
-function add(x, y) {
-    return x + y;
-}
+// function add(x, y) {
+//     return x + y;
+// }
 
+const add = (x, y) => x + y
 /*
  * 2. If a function only has one argument, you can exclude the parentheses.
  *    Convert this to an arrow function that takes advantage of that.
  */
-function increment(number) {
-    return number + 1;
-}
+// function increment(number) {
+//     return number + 1;
+// }
+
+const increment = number => number + 1
 
 /* 
  * 3. If a function's body includes a single expression, you can drop the curly
  *    braces and the return statement. Update this arrow function to be a one
  *    liner which doesn't include "return".
  */
-const decrement = number => {
-    return number - 1;
-};
+const decrement = number => number - 1
+
 
 module.exports = {
     add,
